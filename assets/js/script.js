2313891b35b6596c53fa67d9ff727ca01b4c7dcf
janvipatel7 <janvi72004@gmail.com>
2025-04-02 17:02:01 +0530

    changes in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,57 @@
 let products = [
     {
+        id : 1,
         name : "Gold T-Bar Stud Earrings",
         price : "899",
         image : "./assets/images/product-1.jpg"
     },
     {
+        id : 2,
         name : "Silver Huggie Earrings",
         price : "789",
         image : "./assets/images/product-2.jpg"
     },
     {
+        id : 3,
         name : "Gold Belcher Chain",
         price : "569",
         image : "./assets/images/product-3.jpg"
     },
     {
+        id : 4,
         name : "Silver Eternity Stud Earrings",
         price : "60",
         image : "./assets/images/product-4.jpg"
     },
     
 ]
-
-let cartArr = localStorage.getItem("cart") || [];
 let counter = document.getElementById("counter")
 
+let cartArr = JSON.parse(localStorage.getItem("cart")) || [];
 function addToCart(productId){
-    console.log(productId);
+   
 
     let product =  products.find((item) => {
         return productId == item.id
     })
 
-    let productIdx =  products.findIndex((item) => {
+
+    
+    let productIdx =  cartArr.findIndex((item) => {
         return item.id == productId
     })
-    console.log(productIdx);
-
-    if(productIdx != -1){
-        product[productIdx].quantity++
+    if (productIdx !== -1) {
+        cartArr[productIdx].quantity++;
     }else{
-        product.quantity = 1
-        cartArr.push(product)
+        product.quantity = 1;
+        cartArr.push(product);
     }
+;
+    
 
     localStorage.setItem("cart" , JSON.stringify(cartArr))
     counter.innerHTML = cartArr.length
+   
 }
 
 let productsElement = document.getElementById("product");
@@ -61,7 +67,7 @@ products.forEach((product , idx) => {
         </div>
         <div class="d-flex  justify-content-between">
             <p>$ ${product.price}</p>
-            <button class="bg-color-icon" onclick = "addToCart(${product.id})">
+            <button class="bg-color" onclick = "addToCart(${product.id})">
                 <i class="bi bi-cart3"></i>
             </button>
         </div>
