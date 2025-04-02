a97013e3863e4f1f71c2a9854fb3b5332eafbb24
janvipatel7 <janvi72004@gmail.com>
2025-04-02 13:37:39 +0530

    added images

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,6 +22,32 @@ let products = [
     
 ]
 
+let cartArr = localStorage.getItem("cart") || [];
+let counter = document.getElementById("counter")
+
+function addToCart(productId){
+    console.log(productId);
+
+    let product =  products.find((item) => {
+        return productId == item.id
+    })
+
+    let productIdx =  products.findIndex((item) => {
+        return item.id == productId
+    })
+    console.log(productIdx);
+
+    if(productIdx != -1){
+        product[productIdx].quantity++
+    }else{
+        product.quantity = 1
+        cartArr.push(product)
+    }
+
+    localStorage.setItem("cart" , JSON.stringify(cartArr))
+    counter.innerHTML = cartArr.length
+}
+
 let productsElement = document.getElementById("product");
 products.forEach((product , idx) => {
 
@@ -35,7 +61,9 @@ products.forEach((product , idx) => {
         </div>
         <div class="d-flex  justify-content-between">
             <p>$ ${product.price}</p>
-            <button class="btn btn-danger"> Add To Cart</button>
+            <button class="bg-color-icon" onclick = "addToCart(${product.id})">
+                <i class="bi bi-cart3"></i>
+            </button>
         </div>
         </div>`
 })
