59a5359fad95e04e7bb78553a552d5738bc1c5dc
janvipatel7 <janvi72004@gmail.com>
2025-04-04 16:01:22 +0530

    added cart.html

---
+++ assets/js/cart.js
@@ -0,0 +1,10 @@
+let cartArray = JSON.parse(localStorage.getItem("cart")) || [];
+let showCart = document.getElementById("showcart")
+
+
+function displayCart(){
+
+    cartArray.forEach((item,idx) => {
+        
+    });
+}
